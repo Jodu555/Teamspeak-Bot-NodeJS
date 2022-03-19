3f72417da0592bd7c8372a69e732a5366a778d2d
Jodu555 <Jodu505@gmail.com>
2022-03-19 21:34:47 +0100

    added the online information

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { TeamSpeak, QueryProtocol } = require('ts3-nodejs-library');
 
+const noTeamGroups = ['10', '17', '16', '15', '11']
+
 //create a new connection
 TeamSpeak.connect({
     host: "rooti.jodu555.de",
@@ -11,23 +13,37 @@ TeamSpeak.connect({
     password: process.env.QUERY_PASSWORD,
     nickname: "NodeJS-BOT"
 }).then(async teamspeak => {
-    const buildThereString = async () => {
+    const editOnlineChannels = async () => {
         const clients = await teamspeak.clientList({ clientType: 0 });
-        return `[cspacer]Derzeit ${clients.length == 1 ? 'ist' : 'sind'} ${clients.length} Spieler Online`
+        const teamMembers = clients.filter(c => !c.servergroups.includes(noTeamGroups));
+        let channel = await teamspeak.getChannelById('33');
+        channel.edit({
+            channelName: `[cspacer]Derzeit ${clients.length == 1 ? 'ist' : 'sind'} ${clients.length} Spieler Online`,
+        })
+
+        channel = await teamspeak.getChannelById('34');
+        channel.edit({
+            channelName: `[cspacer]Davon ${clients.length == 1 ? 'ist' : 'sind'} ${teamMembers.length} im Team`,
+        })
     }
 
     const clients = await teamspeak.clientList({ clientType: 0 });
     clients.forEach(client => {
-        console.log("Sending 'Hello!' Message to", client.nickname);
-        // client.message("Hello!")
+        console.log('Online:', client.nickname);
     })
 
-    const channel = await teamspeak.getChannelById('33');
-    console.log(channel);
-    channel.edit({
-        channelName: await buildThereString(),
-    })
 
+
+    teamspeak.on('clientconnect', () => {
+        editOnlineChannels();
+    });
+
+    teamspeak.on('clientdisconnect', () => {
+        editOnlineChannels();
+    });
+
+
+    editOnlineChannels();
 
 
 }).catch(e => {
